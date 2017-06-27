504354b5caf7ae0818dfcd8e890e2858884491fa
Adam Jayne <adamjayne402@gmail.com>
2017-06-27 14:23:02 -0400

    what

---
+++ server/app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var Sequelize = require('sequelize');
+
+var sequelize = new Sequelize('workoutlog', 'postgres', 'It#RainsAllDay3342', {
+	host: 'localhost',
+	dialect: 'postgres'
+});
+// this code will be migrated to a differt file soon.
+sequelize.authenticate().then(
+	function(){
+		console.log('connected to workoutlog postgres db');
+	},
+	function(err){
+		console.log(err);
+	}
+);
+
+app.use(require('./middleware/headers'));
+
+app.use('/api/test', function(req, res){
+	res.send("<h1>Hello World</h1>" + "<h2>It's a small world</h2>");
+});
+
+app.listen(3000, function(){
+	console.log("app is listening on 3000");
+});

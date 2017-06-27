88761a071df9aeacab021c673feb52af746f3262
Adam Jayne <adamjayne402@gmail.com>
2017-06-27 11:27:25 -0400

    added testAPI link

--- client/app.js
+++ client/app.js
@@ -1,15 +1,18 @@
 $(document).ready(function(){
-	$("#testAPI").on("click", function(){
-		console.log("It is working");
+	$("#testAPI").on('click', function(){
+		console.log('hey its working')
 	});
+
 	var test = $.ajax({
-		type:"GET",
-		url:"http://localhost:3000/api/test"
-	});
-	test.done(function(data){
+		type: "GET",
+		url: "http://localhost:3000/api/test"
+	})
+	// .done() = deferred promise
+	.done(function(data){
 		console.log(data);
-	});
-	test.fail(function(){
-		console.log("Oh no!");
+	})
+
+	.fail(function(){
+		console.log("Oops, the server is not responding");
 	});
 });
--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.use('/api/test', function(req, res){
-	res.send("Hello World");
+	res.send("<h1>Hello World</h1>" + "<h2>It's a small world</h2>");
 });
 
 app.listen(3000, function(){

9cb697a51c9e4f8bf34ab6c78a6210752e9342a8
Adam Jayne <adamjayne402@gmail.com>
2017-06-27 17:32:35 -0400

    Setup Routes

--- server/app.js
+++ server/app.js
@@ -1,49 +1,15 @@
 var express = require('express');
 var app = express();
-var Sequelize = require('sequelize');
 var bodyParser = require('body-parser');
-var sequelize = new Sequelize('workoutlog', 'postgres', 'It#RainsAllDay3342', {
-	host: 'localhost',
-	dialect: 'postgres'
-});
-// this code will be migrated to a differt file soon.
-sequelize.authenticate().then(
-	function(){
-		console.log('connected to workoutlog postgres db');
-	},
-	function(err){
-		console.log(err);
-	}
-);
-
-var User = sequelize.define('user', {
-	username: Sequelize.STRING,
-	passwordhash: Sequelize.STRING,
-});
+var sequelize = require('./db.js')
+var User = sequelize.import('./models/user');
 
 User.sync();
 //  User.sync({force:true})
 
 app.use(bodyParser.json());
 
-app.post('/api/user', function(req, res){
-	var username = req.body.user.username;
-	var pass = req.body.user.password;
-	User.create({
-		username: username,
-		passwordhash: ""
-	}).then(
-		function createSuccess(user){
-			res.json({
-				user: user,
-				message: 'create'
-			});
-		},
-		function createError(err){
-			res.send(500, err.message);
-		}
-	);
-});
+app.use('/api/user', require('./routes/user'));
 
 app.use(require('./middleware/headers'));
 

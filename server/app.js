8a1012a512f9add05b9a32318b49e821fda9c63c
Adam Jayne <adamjayne402@gmail.com>
2017-06-27 16:03:05 -0400

    work on stuff now

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var Sequelize = require('sequelize');
-
+var bodyParser = require('body-parser');
 var sequelize = new Sequelize('workoutlog', 'postgres', 'It#RainsAllDay3342', {
 	host: 'localhost',
 	dialect: 'postgres'
@@ -15,6 +15,11 @@ sequelize.authenticate().then(
 		console.log(err);
 	}
 );
+
+var User = seq.define('user', {
+	username: Sequelize.STRING,
+	passwordhash:
+});
 
 app.use(require('./middleware/headers'));
 

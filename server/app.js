1a4ab82c6a24fde468175ee37382cba73ed409a3
Adam Jayne <adamjayne402@gmail.com>
2017-06-28 14:02:59 -0400

    added validation of sessions and password hashing

--- server/app.js
+++ server/app.js
@@ -1,17 +1,22 @@
+// Helps create routes
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var sequelize = require('./db.js')
+var sequelize = require('./db.js');
 var User = sequelize.import('./models/user');
+require('dotenv').config();
 
 User.sync();
 //  User.sync({force:true})
 
 app.use(bodyParser.json());
-
-app.use('/api/user', require('./routes/user'));
-
+// middleware below
 app.use(require('./middleware/headers'));
+app.use(require('./middleware/validate-session'));
+//  Create a user route
+app.use('/api/user', require('./routes/user'));
+//  Log into as a user
+app.use('/api/login', require('./routes/sessions'));
 
 app.use('/api/test', function(req, res){
 	res.send("<h1>Hello World</h1>" + "<h2>It's a small world</h2>");
